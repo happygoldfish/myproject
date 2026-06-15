0568941ee2b66a31062a6345660afedc5d7f1a8b
Carl Johansson <carl.pdal@gmail.com>
2026-06-15 15:25:21 +0200

    Testing tests

--- Tests/test_posts.py
+++ Tests/test_posts.py
@@ -7,25 +7,26 @@ User = get_user_model()
 
 @pytest.fixture
 def post_entry(db):
-    testUser = User.objects.create_user(username='testUser', password='testUser123')
+    testUser = User.objects.create_user(username='testUser123', password='monstermagnet1')
 
-    post = Post(
+    return Post.objects.create(
         title='Test Post', 
         body='Test Body', 
         slug='test_slug', 
         date_released='2026-01-01', 
-        date_updated='2026-02-02', 
+        #date_updated='2026-02-02', 
         banner="default_img.jpg", 
-        author='testUser' 
-    )
-    post.save()
-    return post    
+        author=testUser 
+    ) 
 
 def test_posts_list(client, post_entry):
     ret = client.get('/posts/')
-    breakpoint()
-    """
     assert ret.status_code == 200
     assert b'Test Post' in ret.data
+    
+    """
+    breakpoint()
+    assert ret.status_code == 200
+    
     """
 
