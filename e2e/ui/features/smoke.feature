7b67ce92071b1090c2dab74a8fcfb1612ec975a1
Carl Johansson <carl.pdal@gmail.com>
2026-06-21 11:09:19 +0200

    adding npm test

---
+++ e2e/ui/features/smoke.feature
@@ -0,0 +1,5 @@
+Feature: Smoke
+
+    Scenario: Startsidan går att öppna
+        Given att jag öppnar startsidan
+        Then ska jag se sidans titel innehåller "Home"
---
+++ e2e/ui/steps/smoke.steps.js
@@ -0,0 +1,13 @@
+import { createBdd } from 'playwright-bdd';
+const { Given, When, Then } = createBdd();
+
+Given('att jag öppnar startsidan', async ({ page }) => {
+  await page.goto('/');
+});
+
+Then('ska jag se sidans titel innehåller {string}', async ({ page }, expected) => {
+  const title = await page.title();
+  if (!title.includes(expected)) {
+    throw new Error(`Expected title to include "${expected}" but got "${title}"`);
+  }
+});
